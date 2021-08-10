5f0c3d381b01ee810acfd8fc31a1fdbd8bf3a126
Eric Stimpson <estimpson@fore-thought.com>
2021-08-10 17:47:14 -0400

    Fixed some bugs not evident on codesandbox.io

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,44 +1,49 @@
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import { LinkContainer } from "react-router-bootstrap";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Switch, Route, Link } from 'react-router-dom';
 
 export function MainMenu() {
-  return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <LinkContainer to="/home">
-          <Navbar.Brand>Aztec Supplier Portal</Navbar.Brand>
-        </LinkContainer>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <LinkContainer to="/generate-inventory">
-              <Nav.Link>Generate Inventory</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/reprint-labels">
-              <Nav.Link>Reprint Labels</Nav.Link>
-            </LinkContainer>
-            <LinkContainer to="/generate-asn">
-              <Nav.Link>Generate ASN</Nav.Link>
-            </LinkContainer>
-            <NavDropdown title="Supplier Settings" id="basic-nav-dropdown">
-              <LinkContainer to="/supplier-settings">
-                <NavDropdown.Item>Settings</NavDropdown.Item>
-              </LinkContainer>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+    return (
+        <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    Aztec Supplier Portal
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/generate-inventory">
+                            Generate Inventory
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/reprint-labels">
+                            Reprint Labels
+                        </Nav.Link>
+                        <Nav.Link as={Link} to="/generate-asn">
+                            Generate ASN
+                        </Nav.Link>
+                        <NavDropdown
+                            title="Supplier Settings"
+                            id="basic-nav-dropdown"
+                        >
+                            <NavDropdown.Item as={Link} to="/supplier-settings">
+                                Settings
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">
+                                Something
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
 }
--- src/components/ReprintLabels.tsx
+++ src/components/ReprintLabels.tsx
@@ -1,81 +1,88 @@
-import Card from "react-bootstrap/Card";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Table from "react-bootstrap/Table";
-import FormControl from "react-bootstrap/FormControl";
-import FormCheck from "react-bootstrap/FormCheck";
-import FormInputSelect from "./forms/FormInputSelect";
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Table from 'react-bootstrap/Table';
+import FormControl from 'react-bootstrap/FormControl';
+import FormCheck from 'react-bootstrap/FormCheck';
+import FormInputSelect from './forms/FormInputSelect';
 
 // Importing the Bootstrap CSS
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import SupplisrLotList from "../data/SupplierLotList";
-const supplierLotList = SupplisrLotList();
+import SupplierLotList from '../data/SupplierLotList';
+import PreObjectList from '../data/PreObjectList';
 
-import PreObjectList from "../data/PreObjectList";
-const preObjectList = PreObjectList("lot");
+const supplierLotList = SupplierLotList();
+const preObjectList = PreObjectList('lot');
 
 export default function ReprintLabels() {
-  return (
-    <>
-      <Container>
-        <h1 className="header">Reprint Labels</h1>
-        <Card>
-          <Card.Body>
-            <Card.Title>
-              Select a previous lot number and select labels to reprint.
-            </Card.Title>
-            <Form>
-              <FormInputSelect
-                controlId="formSupplierParts"
-                label="Lot:"
-                values={supplierLotList.map(
-                  (supplierLot) => supplierLot.lotNumber
-                )}
-              ></FormInputSelect>
-            </Form>
-          </Card.Body>
-        </Card>
-        <Card className="mt-5">
-          <Card.Body>
-            <Card.Title>
-              (Optionally) Edit the inventory in this batch to adjust
-              quantities. Then select and print labels.
-            </Card.Title>
-            <Form>
-              <Table striped bordered hover size="sm" variant="dark">
-                <thead>
-                  <tr>
-                    <th className="text-center">
-                      <FormCheck type="checkbox" />
-                    </th>
-                    <th>Serial</th>
-                    <th>Lot</th>
-                    <th>Qty per Object</th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {preObjectList.map((preObject) => (
-                    <tr key={preObject.serial}>
-                      <td className="text-center">
-                        <FormCheck type="checkbox" />
-                      </td>
-                      <td>{preObject.serial}</td>
-                      <td>{preObject.lotNumber}</td>
-                      <td>{preObject.quantity}</td>
-                      <td>Edit</td>
-                    </tr>
-                  ))}
-                </tbody>
-              </Table>
-            </Form>
-          </Card.Body>
-        </Card>
-      </Container>
-    </>
-  );
+    return (
+        <>
+            <Container>
+                <h1 className="header">Reprint Labels</h1>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>
+                            Select a previous lot number and select labels to
+                            reprint.
+                        </Card.Title>
+                        <Form>
+                            <FormInputSelect
+                                controlId="formSupplierParts"
+                                label="Lot:"
+                                values={supplierLotList.map(
+                                    (supplierLot) => supplierLot.lotNumber,
+                                )}
+                            ></FormInputSelect>
+                        </Form>
+                    </Card.Body>
+                </Card>
+                <Card className="mt-5">
+                    <Card.Body>
+                        <Card.Title>
+                            (Optionally) Edit the inventory in this batch to
+                            adjust quantities. Then select and print labels.
+                        </Card.Title>
+                        <Form>
+                            <Table
+                                striped
+                                bordered
+                                hover
+                                size="sm"
+                                variant="dark"
+                            >
+                                <thead>
+                                    <tr>
+                                        <th className="text-center">
+                                            <FormCheck type="checkbox" />
+                                        </th>
+                                        <th>Serial</th>
+                                        <th>Lot</th>
+                                        <th>Qty per Object</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {preObjectList.map((preObject) => (
+                                        <tr key={preObject.serial}>
+                                            <td className="text-center">
+                                                <FormCheck type="checkbox" />
+                                            </td>
+                                            <td>{preObject.serial}</td>
+                                            <td>{preObject.lotNumber}</td>
+                                            <td>{preObject.quantity}</td>
+                                            <td>Edit</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </Table>
+                        </Form>
+                    </Card.Body>
+                </Card>
+            </Container>
+        </>
+    );
 }
